7b35aae88692b6b1e118375d181d65d4c029b0ae
lvu237it <luuvanvua7k16vt@gmail.com>
2025-07-06 22:46:26 +0700

    Add cronjob to keep server alive

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const { connectDB } = require('./db');
 const errorHandler = require('./middlewares/errorHandler');
 
+require('./utils/cronJob');
+
 // Import Swagger
 const swaggerUi = require('swagger-ui-express');
 // Load your swagger.json (which should reference /auth and /users, not /api/auth)
@@ -26,11 +28,24 @@ const PORT = process.env.PORT || 3000;
 // Middleware để parse JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+
+//Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('production environment mode');
+}
+
+let apiBaseUrl = `http://localhost:${PORT}`;
+if (process.env.NODE_ENV === 'production') {
+  apiBaseUrl = 'https://do-khong-truot-phat-nao.onrender.com';
+}
 
 // Cấu hình Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-console.log(`Xem chi tiết tài liệu api tại http://localhost:${PORT}/api-docs`); // Thêm log để dễ dàng truy cập
+console.log(`Xem chi tiết tài liệu api tại ${apiBaseUrl}/api-docs`); // Thêm log để dễ dàng truy cập
 
 // Import Routes
 const userRoutes = require('./routes/user.routes');
@@ -50,6 +65,14 @@ app.use('/api/notifications', notificationRoutes);
 app.use('/api/auth', authRoutes);
 // app.use('/api/admin', adminRoutes);
 
+//-----------------Test deploying---------------------
+app.use('/test', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Deploy thành công gòi nhe 😸😸😸!',
+  });
+});
+
 // Error handling middleware
 app.use(errorHandler);
 
@@ -58,7 +81,7 @@ connectDB()
   .then(() => {
     // Khởi động server
     app.listen(PORT, () => {
-      console.log(`🚗 Server đang chạy tại http://localhost:${PORT}`);
+      console.log(`🚗 Server đang chạy tại ${apiBaseUrl}`);
     });
   })
   .catch((err) => {
